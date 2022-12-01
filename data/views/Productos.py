5c93c7ef3a3cb5fc31fba8405d67b63073f67e25
Merlinys <martinlucianofonsecarubio@gmail.com>
2022-12-01 12:40:44 -0300

    Cambios registro producto V2

--- data/views/Productos.py
+++ data/views/Productos.py
@@ -12,12 +12,12 @@ def agregarProducto(request):
             tp = TipoProd.objects.get(tp_id=request.POST['tipoProd'])
             prod_nombre = request.POST['nombre']
             prod_precio = request.POST['precio']
-            prod_disponible = request.POST['prodDis']
             prod_fecha_ingreso = request.POST['fechaingreso']
             try:
                 Prod = Producto.objects.get(prod_id = prod_id)
             except Producto.DoesNotExist:
-                Producto(prod_id = prod_id,admin_rut=admin_rut,tp =tp,prod_nombre =prod_nombre,prod_precio =prod_precio,prod_disponible = prod_disponible,prod_fecha_ingreso = prod_fecha_ingreso).save()
+                Producto(prod_id = prod_id,admin_rut=admin_rut,tp =tp,prod_nombre =prod_nombre,prod_precio =prod_precio,prod_disponible = 1,prod_fecha_ingreso = prod_fecha_ingreso).save()
+                messages.success(request,"El producto se ingreso de forma correcta ")
                 return render(request,"addProducto.html", {},status=200)
             messages.error(request,'El producto ingresado ya existe')
             return render(request,"addProducto.html", {},status=200)
