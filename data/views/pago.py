2ecd4efae9e317cb7853552aa66cd4ee26a3e4a4
Martin Fonseca <martinlucianofonsecarubio@gmail.com>
2022-12-11 10:11:06 -0300

    select a input

--- data/views/pago.py
+++ data/views/pago.py
@@ -11,16 +11,39 @@ def cancelarComanda(request):
             clienteexist= Cliente.objects.get(cli_rut=request.POST['Rut_Cliente'])
         except Cliente.DoesNotExist:
             messages.error(request,"Rut Cliente incorrecto")
-            return render (request,"payment.html")
-        try:
-            garzon = Garzon.objects.get(gar_rut=request.POST['Garzon_Rut'])
-        except Garzon.DoesNotExist:
-            messages.error(request,"Rut Garzon incorrecto")
-            return render (request,"payment.html")
+            return render(request,"payment.html")
+        
+       
         try:
             comanda = Comanda.objects.get(com_id=request.POST['idComanda'])
         except Comanda.DoesNotExist:
             messages.error(request,"ID Comanda incorrecto")
-            return render (request,"payment.html")
+            return render(request,"payment.html") 
+        
+        #Comprueba si ya existe una venta con ese id
+        if Venta.objects.filter(ven_id = request.POST['id venta']):
+            messages.error(request,"ID venta incorrecto")
+            return render(request,"payment.html")
+        
+        #Variables
+        metodoPago = TipoPago.objects.get(tpag_id = int(request.POST.get('metodo')))
+        bancos = Banco.objects.get(banco_id= int(request.POST.get('Banco')))
+        
+        Venta(ven_id = request.POST['id venta'],ven_fecha=datetime.date.today()).save()
+        coma = Comanda.objects.get(com_id=request.POST['idComanda'])
+        coma.ven = Venta.objects.get(ven_id = request.POST['id venta'])
+        coma.save()
+        if metodoPago.tpag_id ==2:
+            Pago(pago_id = request.POST['id venta'],banco = bancos,ven = Venta.objects.get(ven_id = request.POST['id venta']),tpag =metodoPago,pago_fecha_hora = datetime.datetime.today(),pago_nro_boucher = request.POST['boucher'],pago_monto = request.POST['Precio']  ).save()
+        else:
+            Pago(pago_id = request.POST['id venta'],ven = Venta.objects.get(ven_id = request.POST['id venta']),tpag =metodoPago,pago_fecha_hora = datetime.datetime.today(),pago_nro_boucher = request.POST['boucher'],pago_monto = request.POST['Precio']  ).save()
+
+        messages.success("Pago realizado correctamente")
+        return render(request,"payment.html")
+        
+        
+        
+            
+        
         
         
